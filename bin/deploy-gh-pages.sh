984149c9af8279023d68a67fbe1700f54a1ddca7
Lee Byron <lee@leebyron.com>
2015-04-01 19:36:29 -0700

    not so quiet

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -8,4 +8,4 @@ git config user.name "Travis CI"
 git config user.email "lee@leebyron.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/leebyron/spec-md.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@github.com/leebyron/spec-md.git" master:gh-pages
