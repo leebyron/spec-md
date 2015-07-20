01778d415303c36a4dd9f4f840ebe014e11fdf2b
Travis CI <lee@leebyron.com>
2015-07-20 15:14:20 -0700

    fix formatting issues

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-var beautify = require('js-beautify');
 var print = require('./print');
 var parse = require('./parse');
 var visit = require('./visit');
 
 function html(filepath, options) {
   return parse(filepath).then(function (ast) {
-    var rawHTML = print(ast, options);
-    return beautify.html(rawHTML, {
-      indent_size: 2,
-    });
+    return print(ast, options);
   });
 }
 
