8ea1c8d39afedae7af3e49b274307e7438c25cd9
Travis CI <lee@leebyron.com>
2017-09-20 16:47:43 -0700

    Add useful error message when document title is missing
    Fixes #13

--- src/parse.js
+++ src/parse.js
@@ -9,7 +9,9 @@ var visit = require('./visit');
 module.exports = parse;
 
 function parse(filepath) {
-  return importAST(grammar, filepath);
+  return importAST(grammar, filepath).then(function (ast) {
+    return validate(filepath, ast);
+  });
 }
 
 function readFile(filepath) {
@@ -40,26 +42,33 @@ function importAST(parser, filepath) {
       }
     });
     return Promise.all(importASTs).then(function (asts) {
-      var cursor = 0;
-      var needToFlattenStack = [];
-      var needToFlatten;
-      return visit(ast, {
-        leave: function (node, key, parent, keyPath) {
-          if (needToFlatten !== undefined && keyPath.join('.') === needToFlatten) {
-            node.contents = node.contents.reduce(flattener, []);
-            needToFlatten = needToFlattenStack.pop();
-          }
-          if (node.type === 'Import') {
-            var pathToFlatten = keyPath.slice(0, -1).join('.');
-            if (pathToFlatten !== needToFlatten) {
-              needToFlattenStack.push(needToFlatten);
-              needToFlatten = pathToFlatten;
-            }
-            return asts[cursor++].contents;
-          }
-        }
-      });
+      return flattenDocuments(ast, asts);
     });
+  });
+}
+
+/**
+ * Returns a single AST from a collection of ASTs.
+ */
+function flattenDocuments(ast, asts) {
+  var cursor = 0;
+  var needToFlattenStack = [];
+  var needToFlatten;
+  return visit(ast, {
+    leave: function (node, key, parent, keyPath) {
+      if (needToFlatten !== undefined && keyPath.join('.') === needToFlatten) {
+        node.contents = node.contents.reduce(flattener, []);
+        needToFlatten = needToFlattenStack.pop();
+      }
+      if (node.type === 'Import') {
+        var pathToFlatten = keyPath.slice(0, -1).join('.');
+        if (pathToFlatten !== needToFlatten) {
+          needToFlattenStack.push(needToFlatten);
+          needToFlatten = pathToFlatten;
+        }
+        return asts[cursor++].contents;
+      }
+    }
   });
 }
 
@@ -69,4 +78,36 @@ function flattener(array, item) {
   }
   array.push(item);
   return array;
+}
+
+/**
+ * Throw useful error message if a complete AST contains any errors.
+ */
+function validate(filepath, ast) {
+  return visit(ast, {
+    enter(node) {
+      if (node.type === 'Document') {
+        validateDocument(filepath, node);
+      }
+    }
+  });
+}
+
+function validateDocument(filepath, ast) {
+  if (!ast.title) {
+    var message =
+      'Primary document ' + filepath + ' is missing a title. ' +
+      'It must begin with a Setext-style header. Example:\n\n' +
+      'Title of Spec\n' +
+      '=============\n';
+
+    var firstItem = ast.contents && ast.contents[0];
+    if (firstItem && firstItem.type === 'Section') {
+      message +=
+        '\nDid you mean to use Setext-style for the first title?\n\n' +
+        '# ' + firstItem.title + '\n';
+    }
+
+    throw new Error(message);
+  }
 }
