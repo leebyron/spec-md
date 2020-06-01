e897c9eb703bf9b3ef7a2e519465fbb1ecccc109
Benjie Gillam <benjie@jemjie.com>
2020-06-01 19:56:54 +0100

    Newlines after tds

--- src/print.js
+++ src/print.js
@@ -574,7 +574,7 @@ function printAll(list, options) {
                   return (
                     '<tr>\n' +
                       join(row.map(function (def) {
-                        return '<td class="spec-rhs">' + def + '</td>';
+                        return '<td class="spec-rhs">' + def + '</td>\n';
                       })) +
                     '</tr>\n'
                   );
