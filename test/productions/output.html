leebyron/spec-md

commit 10dde32be45d5cbe87072bcf0ef0df5d38d043ff

Unescape dollars (#55)

src/grammar.pegjs
@@ -13,7 +13,7 @@
   }
 
   function unescape(text) {
-    return text.replace(/\\([\\`*_{}[\]()#+\-.!<>|])/g, '$1');
+    return text.replace(/\\([\\`*_{}[\]()#+\-.!<>|$])/g, '$1');
   }
 
   let htmlBlockName;

commit 4f5c6e79e21ab06e31261dc3f0ad9a9e3097089d

Use existing __ and Line rules, fixing an issue introduced by #55. Adds tests

src/grammar.pegjs
@@ -616,10 +616,10 @@ production = BLOCK token:nonTerminal _ defType:(':::'/'::'/':') rhs:productionRH
 
 productionRHS = oneOfRHS / singleRHS / listRHS
 
-oneOfRHS = !(LINE listBullet) __ 'one of' WB NL_NO_BULLET? rows:(__ (NL _ listBullet?)? (_ token)+)+ {
+oneOfRHS = !(LINE listBullet) __ 'one of' WB rows:((LINE listBullet / __)? (_ token)+)+ {
   return {
     type: 'OneOfRHS',
-    rows: rows.map(row => row[2].map(tokens => tokens[1]))
+    rows: rows.map(row => row[1].map(tokens => tokens[1]))
   };
 }
 
@@ -807,7 +807,6 @@ DEDENT = &lineStart !{ indentStack.length === 0 } {
 NL = '\n' / '\r' / '\r\n'
 NOT_NL = [^\n\r]
 SINGLE_NL = NL !(NL / _ listBullet)
-NL_NO_BULLET = NL !(_ listBullet)
 _ = ' '*
 // Skips over whitespace including a single newline. Do not use more than once
 // in a row, otherwise multiple NL will be skipped.

test/productions/output.html
@@ -1075,6 +1075,22 @@ pre[class*="language-"] {
 <div class="spec-production" id="PBJ">
 <span class="spec-nt"><a href="#PBJ" data-name="PBJ">PBJ</a></span><div class="spec-rhs"><span class="spec-nt"><span data-name="Bread">Bread</span></span><span class="spec-constrained"><span class="spec-nt"><span data-name="Condiment">Condiment</span></span><span class="spec-butnot"><span class="spec-nt"><span data-name="Ketchup">Ketchup</span></span><span class="spec-nt"><span data-name="Mustard">Mustard</span></span></span></span><span class="spec-nt"><span data-name="Bread">Bread</span></span></div>
 </div>
+<p>Multiple definitions can have a newline after it</p>
+<div class="spec-production" id="PBJ">
+<span class="spec-nt"><a href="#PBJ" data-name="PBJ">PBJ</a></span><div class="spec-rhs"><span class="spec-nt"><span data-name="Bread">Bread</span></span><span class="spec-nt"><span data-name="PeanutButter">PeanutButter</span></span><span class="spec-nt"><span data-name="Jelly">Jelly</span></span><span class="spec-nt"><span data-name="Bread">Bread</span></span></div>
+<div class="spec-rhs"><span class="spec-nt"><span data-name="Bread">Bread</span></span><span class="spec-nt"><span data-name="PeanutButter">PeanutButter</span></span><span class="spec-nt"><span data-name="Jelly">Jelly</span></span><span class="spec-nt"><span data-name="Bread">Bread</span></span></div>
+</div>
+<p>One of can have a newline after it</p>
+<div class="spec-production" id="Punc">
+<span class="spec-nt"><a href="#Punc" data-name="Punc">Punc</a></span><div class="spec-oneof"><div class="spec-oneof-grid"><table>
+<tr>
+<td class="spec-rhs"><span class="spec-t">.</span></td></tr>
+<tr>
+<td class="spec-rhs"><span class="spec-t">!</span></td></tr>
+<tr>
+<td class="spec-rhs"><span class="spec-t">?</span></td></tr>
+</table></div></div>
+</div>
 </section>
 <section id="sec-Algorithms" secid="2">
 <h1><span class="spec-secid" title="link to this section"><a href="#sec-Algorithms">2</a></span>Algorithms</h1>
@@ -1103,7 +1119,7 @@ pre[class*="language-"] {
 </ol>
 </div>
 </section>
-<section id="index" secid="index" class="spec-index"><h1><span class="spec-secid" title="link to the index"><a href="#index">§</a></span>Index</h1><ol><li><a href="#Algo()">Algo</a></li><li><a href="#PBJ">PBJ</a></li><li><a href="#Sandwich">Sandwich</a></li></ol></section></article>
+<section id="index" secid="index" class="spec-index"><h1><span class="spec-secid" title="link to the index"><a href="#index">§</a></span>Index</h1><ol><li><a href="#Algo()">Algo</a></li><li><a href="#PBJ">PBJ</a></li><li><a href="#Punc">Punc</a></li><li><a href="#Sandwich">Sandwich</a></li></ol></section></article>
 <footer>
 Written in <a href="https://spec-md.com" target="_blank">Spec Markdown</a>.</footer>
 <input hidden class="spec-sidebar-toggle" type="checkbox" id="spec-sidebar-toggle" aria-hidden /><label for="spec-sidebar-toggle" aria-hidden><div class="spec-sidebar-button">&#x2630;</div></label>